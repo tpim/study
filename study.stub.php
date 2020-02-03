adf8eface9888eee2a003e19460b03c98081b560
tpim <qingcongke@gmail.com>
2020-02-03 10:34:21 +0000

    basic plugin skel

---
+++ study.stub.php
@@ -0,0 +1,5 @@
+<?php
+
+function study_test1(): void {}
+
+function study_test2(string $str = ""): string {}
---
+++ study_coroutine.h
@@ -0,0 +1,9 @@
+#include "php_study.h"
+
+namespace Study
+{
+	class PHPCoroutine
+	{
+		static long create(zend_fcall_info_cache *fci_cache, uint32_t argc, zval *argv);
+	}
+}
